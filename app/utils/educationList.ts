de15621e50ad058a00fa8af8d7429a1b052a31cb
森淼 <wincatcher@qq.com>
2024-11-02 23:53:27 +0800

    Update educationList.ts

--- app/utils/educationList.ts
+++ app/utils/educationList.ts
@@ -2,13 +2,8 @@ import { educationType } from "../types";
 
 export const educationList: educationType[] = [
   {
-    career: "Full Stack Developmet Immersive",
-    years: "2022",
-    description: "",
-  },
-  {
-    career: "Computer Engineering",
-    years: "2018-2021",
+    career: "机电工程与自动化",
+    years: "",
     description: "",
   },
 ];
