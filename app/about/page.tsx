9b79f4ee75a0661094d0c4b9745d7c753db4fdc1
森淼 <wincatcher@qq.com>
2024-11-03 01:17:57 +0800

    修复布局错误

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,5 +2,9 @@
 import { AboutContent } from "../components/shared/AboutContent";
 
 export default function About() {
-  return <AboutContent variant="page" />;
+  return (
+    <main className="p-4 pb-12 max-w-6xl m-auto overflow-hidden 2xl:overflow-visible">
+      <AboutContent variant="page" />
+    </main>
+  );
 }
